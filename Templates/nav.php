d64cec802a44bbc8f5a1a6cc247e76ad5ea0325a
waniniRan <cindy.gichuhi@strathmored.edu>
2024-06-14 09:58:56 +0300

    add nav.php

---
+++ Templates/nav.php
@@ -0,0 +1,7 @@
+<nav class="navbar">
+    <a href="#">Home</a> 
+    <a href="#">About Us</a> 
+    <a href="#">Products</a> 
+    <a href="#">Reviews </a>  
+    <a href="#">Contact</a> 
+   </nav>
