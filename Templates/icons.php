27f729f1490a209f151f74f23e6837686ba114e4
waniniRan <cindy.gichuhi@strathmored.edu>
2024-06-14 10:08:22 +0300

    add icons php

---
+++ Templates/icons.php
@@ -0,0 +1,6 @@
+<div class="icons">
+  <a href="#" class="fas fa-heart"></a>
+  <a href="#" class="fas fa-shopping-cart"></a>
+  <a href="#" class="fas fa-user-alt"></a>
+
+</div>
--- index.php
+++ index.php
@@ -24,17 +24,11 @@
     <a href="#" class="logo">GardenGrace<span>.</span></a>
 
 
-   <?php  include_once(Templates/"nav.php"); ?>
+   <?php  include_once("Templates/nav.php"); ?>
 
 
 
-<div class="icons">
-  <a href="#" class="fas fa-heart"></a>
-  <a href="#" class="fas fa-shopping-cart"></a>
-  <a href="#" class="fas fa-user-alt"></a>
-
-</div>
-
+<?php include_once("Templates/icons.php"); ?>
   </header>
 
   <!--header section ends here-->  
