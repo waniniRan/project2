9ffe62012906a631b244658bd7bc4b2bf97a6fb5
waniniRan <cindy.gichuhi@strathmored.edu>
2024-07-08 10:32:25 +0300

    add new lines

--- about.php
+++ about.php
@@ -1,45 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE-edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Flowers</title>
-  
-  <!--font awsesome cdn link-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" >
-<!--custom css file link-->
-
-<link rel="stylesheet" href="CSS/style.css">
-</head>
-<body>
-
-  <!--header section starts here-->
-
-  <header>
-
-   <input type="checkbox" name=" " id="toggler">
-   <label for="toggler" class="fas fa-bars"></label>
-
-   <a href="#" class="logo">GardenGrace<span>.</span></a>
+<?php  include_once("Templates/heading.php"); ?>
    
-
-
-   <?php  include_once("Templates/nav.php"); ?>
-   
-   <?php include_once("Templates/icons.php"); ?>
-
-  </header>
-  <!--header section ends here-->
-
-
-  <!--home section starts here-->
 
 <section class="home" id="home">
   <div class="content">
-
-  <!--home section ends here-->
-
 <!--about section starts -->
 
 <section class="about" id="about">
@@ -60,30 +23,9 @@
 </section>
 
 <!--about section ends here-->
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
 <div>
 </div>
 
   <?php include_once ("Templates/footer.php"); ?>
-
-
-
-
-  
 </body>
 </html>
-
-
