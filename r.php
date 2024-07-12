a18242554bee294ee28fbd60c498bdb1015f6c22
waniniRan <cindy.gichuhi@strathmored.edu>
2024-07-12 10:11:45 +0300

    new lines

--- r.php
+++ r.php
@@ -5,9 +5,9 @@
 /*the product and review page*/
 $product = [
     'name' => 'Rose Bouquet',
-    'price' => 3000ksh,
+    'price' => '3000',
     'description' => 'A beautiful bouquet of fresh roses.',
-    'img'=>\Images\download.webp
+    'img'=>'\Images\download.webp'
 ];
 include_once("Templates/heading.php");
 ?>
@@ -19,7 +19,7 @@ include_once("Templates/heading.php");
             <div class="product">
                 <img src="<?php echo $product['img']; ?>" alt="<?php echo $product['name']; ?>">
                 <h2><?php echo $product['name']; ?></h2>
-                <p class="price">$<?php echo number_format($product['price'], 2); ?></p>
+                <p class="price">KSH<?php echo number_format($product ['price'], 2); ?></p>
                 <p class="description"><?php echo $product['description']; ?></p>
                 <button>Add to Cart</button>
             </div>
