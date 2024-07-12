984d364e1541a719e91bcddc83c527df8e4d0541
waniniRan <cindy.gichuhi@strathmored.edu>
2024-07-12 09:43:26 +0300

    remove box-contanier

--- Templates/footer.php
+++ Templates/footer.php
@@ -1,14 +1,4 @@
 <section class="footer">
-
-<div class="box-container">
-  <div class="box">
-   <h3>Quick Links</h3>
-   <a href="index.php">Home</a>
-   <a href="about.php">About</a>
-   <a href="view_messages.php">View Message</a>
-   <a href="r.php">Products</a>
-    <a href="contacts.php">Contacts</a>
-  </div>
       <div class="credit">
             Copyright  &copy;GardenGrace <?php print date("Y"); ?> - Allrights Reserved 
         </div>   
